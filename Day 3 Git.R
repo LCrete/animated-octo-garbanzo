c8a4389f3c54ec0c36412e3a0b1c46747bb1b0e8
Lucile Crété <crete.lucile@gmail.com>
2020-01-15 11:56:52 +0000

    New script

---
+++ Day 3 Git.R
@@ -0,0 +1,27 @@
+#Day 3 = reproducible research
+#see british ecological society guide to reproducibility https://www.britishecologicalsociety.org/wp-content/uploads/2019/06/BES-Guide-Reproducible-Code-2019.pdf
+
+#version
+#file names: best option is year-month-day_project
+#or use version control in Git
+
+
+#### Git workskflow ####
+# Git saves all versions of work on computer
+#1. Set up a repository
+#2. Add files to track
+#3. Commit = commit latest change to the repo
+#4. Modify file (with comment explaining what changed), save it, then add file and commit to repo
+# ==> doing that then the only file you see in Git is the latest one, but all versions are stored within Git
+
+#GitHub: webserver that saves everything on the cloud, not only on computer
+#https://github.com/LCrete/animated-octo-garbanzo.git
+
+
+
+
+
+
+
+library(tidyverse)
+read.csv()
